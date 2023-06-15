b09930f6434d05ebb15a59c1aa08209c0133134c
Abror Musulmonov <116016090+AbrorMusulmonov@users.noreply.github.com>
2023-06-15 17:45:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Abstraction_and_Encupsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Abituriyent klasiga mansub abit obektini e'lon qilamiz:
+            Abituriyent abit =new Abituriyent();
+            //abit obyektini Ball xossasiga ko'ra 90 qiymatini belgilaymiz:
+            abit.Bal = 90;
+            //abit obyektini Ball xossasiga ko'ra konsolga chiqaramiz:
+            Console.WriteLine(abit.Bal);
+            //abit obyektini Ball xossasiga ko'ra 120 qiymatini belgilaymiz:
+            abit.Bal = 120;
+            // Console.WriteLine(abit.Bal);
+            Console.Read();
+            abit.takeExam();//dynamic method-ordinary method
+            Abituriyent.register();//static method
+            /*Abstraction_and_Encupsulation - buning asosiy vazifasi user uchun faqat kerakli qismlarni ko'rsatishdan iboratdir*/
+
+        }
+    }
+}
